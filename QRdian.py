77b3045e707541388f9c6241aa8cdaa3ec183c1d
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-10-29 02:50:51 +0400

    start

--- QRdian.py
+++ QRdian.py
@@ -1,6 +1,8 @@
 # pip3 install pygame==2.0.0.dev12
-#pip3 install pyzbar
+# pip3 install pyzbar
+# pip3 install opencv-python
 # sudo apt-get install git curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0
+# sudo apt-get install -y build-essential libzbar-dev
 
 import pygame.camera
 from pyzbar import pyzbar
@@ -11,14 +13,14 @@ import threading
 w = 640
 h = 480
 
-pygame.camera.init()
 
 
 def routine(*args):
     cam = pygame.camera.Camera("/dev/video0", (w, h))
     print(1)
-    cam.start()
     while 1:
+        pygame.camera.init()
+        cam.start()
         print(2)
         image = cam.get_image()
         print(4)
@@ -29,6 +31,7 @@ def routine(*args):
         code = pyzbar.decode(code_image)
         print(code)
         time.sleep(0.5)
+        cam.stop()
 
 
 

90efdd88f04050a6cded81a9c59307bc056b0ec9
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-10-30 01:50:34 +0400

    hj

--- QRdian.py
+++ QRdian.py
@@ -12,56 +12,12 @@
 # sudo apt-get install libqtgui4
 # sudo apt-get install libqt4-test
 
-
-# import pygame.camera as camera
-# import pygame.image as image
-# from pyzbar import pyzbar
-# from PIL import Image
-# import time
-#
-# import threading
-#
-# w = 640
-# h = 480
-#
-# camera.init()
-#
-#
-# def routine(*args):
-#     cam_list = camera.list_cameras()
-#     print(cam_list)
-#     cam = camera.Camera(cam_list[0], (w, h))
-#     print(1)
-#     cam.start()
-#     while 1:
-#         time.sleep(2)
-#         print(2)
-#         img = cam.get_image()
-#         print(4)
-#         img = image.tostring(img, "RGBA", False)
-#         print(5)
-#         code_image = Image.frombuffer("RGBA", (w, h), img)
-#         print(6)
-#         code = pyzbar.decode(code_image)
-#         print(code)
-#
-#
-#
-# # x = threading.Thread(target=routine, args=(None,))
-# # x.start()
-# # while 1:
-# #     time.sleep(1)
-#
-# routine('q')
-
 import cv2
-from pyzbar import pyzbar
 
-cam = cv2.VideoCapture(1)
 cam = cv2.VideoCapture(0)
 s, code_image = cam.read()
 detector = cv2.QRCodeDetector()
 data = detector.detectAndDecode(code_image)
 print(data)
-
+cam.release()
 

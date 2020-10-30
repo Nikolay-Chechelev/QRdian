e41edd75fa3f3d2fa33eb5cf4acee54dfd11bc32
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-10-30 04:10:22 +0400

    hj

--- QRdian.py
+++ QRdian.py
@@ -19,10 +19,10 @@ from PIL import Image
 from pyzbar import pyzbar
 import os, time
 
-os.system('sudo rmmod uvcvideo')
-time.sleep(2)
-os.system('sudo modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80')
-time.sleep(2)
+# os.system('sudo rmmod uvcvideo')
+# time.sleep(2)
+# os.system('sudo modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80')
+# time.sleep(2)
 
 camera.init()
 
@@ -30,13 +30,14 @@ cam_list = camera.list_cameras()
 
 cam = camera.Camera(cam_list[0], (640, 480))
 cam.start()
-
+i = 0
 #cam.stop()
 while 1:
+    i += 1
     time.sleep(1)
     cam.get_image()
     img = cam.get_image()
     img = image.tostring(img, "RGBA", False)
     img = Image.frombytes("RGBA", (640, 480), img)
     code = pyzbar.decode(img)
-    print(code)
+    print(i, code)
